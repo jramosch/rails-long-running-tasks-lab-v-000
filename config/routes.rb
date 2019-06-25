91786fe0715d5cea56b56a06c5cf7e8ae446c6e9
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2019-06-25 20:47:39 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+<<<<<<< HEAD
   resources :songs
   resources :artists
+=======
+  resources :songs, only: [:index]
+>>>>>>> b592595e7f13dfe62174ceec7c3dbfc0a8cdd08f
   post 'songs/upload'
 end
